045dedb74b007b988bdf026215b5442d3bef98ca
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2026-02-03 21:47:43 +0545

    added middleware to protect admin and user routes

--- lib/authApi.ts
+++ lib/authApi.ts
@@ -1,11 +1,11 @@
 import api from "./axios";
 
 export const registerUser = async (payload: any) => {
-  const res = await api.post("/api/auth/register", payload);
+  const res = await api.post("/api/auth/register", payload); // ✅ FIX
   return res.data;
 };
 
 export const loginUser = async (payload: any) => {
-  const res = await api.post("/api/auth/login", payload);
+  const res = await api.post("/api/auth/login", payload); // ✅ FIX
   return res.data;
 };
